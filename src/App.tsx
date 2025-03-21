9e49501337e7da5fa98919826655abdfae5e7b42
Sheyne Anderson <sheyne@sheyne.com>
2025-03-20 20:17:53 -0700

    Make the buzzle website look fun?

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,79 @@
-import { Game } from "./game";
+import { Game, ordered } from "./game";
 import useLocalStorageState from "use-local-storage-state";
 import { minutesAndSeconds, fromMinutesAndSeconds } from "./minutesAndSeconds";
 import "./App.css";
 
+const minNumberPlayers = 4;
+
+function cleanupString(x: string): string {
+  while (x.startsWith(" ")) {
+    x = x.slice(1);
+  }
+  while (x.endsWith(" ")) {
+    x = x.slice(0, x.length - 1);
+  }
+  return x;
+}
+
+function hash(x: string) {
+  var hash = 0,
+    i,
+    chr;
+  if (x.length === 0) return hash;
+  for (i = 0; i < x.length; i++) {
+    chr = x.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+function colorOf(player1: string, player2: string) {
+  return `hsl(${(hash(JSON.stringify([player1, player2])) / 12345) * 69}deg 70% 70%)`;
+}
+function rotation(nonce: string) {
+  return `${Math.sin(hash(nonce) * 1.69) * 3.14}deg`;
+}
+
+function Card(args: {
+  playersAndTime: [string, string, number];
+  rank: number;
+}) {
+  const [p1, p2, time] = args.playersAndTime;
+  return (
+    <div
+      className={`card card-${args.rank}`}
+      style={{
+        backgroundColor: colorOf(p1, p2),
+        rotate: rotation(JSON.stringify(args)),
+      }}
+    >
+      <div className="rank">{args.rank}</div>
+      <div className="times">
+        <div>{p1}</div>
+        <div>&</div>
+        <div>{p2}</div>
+      </div>
+      <div className="time">{minutesAndSeconds(time)}</div>
+    </div>
+  );
+}
+
 function Leaderboard({ times }: { times: [string, string, number][] }) {
   return (
     <div className="leaderboard">
-      <h2>Leaderboard</h2>
-      <ol>
-        {times.map(([p1, p2, time]) => (
-          <li>
-            {p1} & {p2}: {minutesAndSeconds(time)}
-          </li>
+      <div className="card-container">
+        <Card playersAndTime={times[0]} rank={1} />
+      </div>
+      <div className="card-container">
+        <Card playersAndTime={times[1]} rank={2} />
+        <Card playersAndTime={times[2]} rank={3} />
+      </div>
+      <div className="card-container">
+        {times.slice(3).map((playersAndTime, idx) => (
+          <Card playersAndTime={playersAndTime} rank={idx + 4} />
         ))}
-      </ol>
+      </div>
     </div>
   );
 }
@@ -23,7 +83,7 @@ function Pairing({
   players: [player1, player2],
 }: {
   title: string;
-  players: [string, string];
+  players: readonly [string, string];
 }) {
   return (
     <div className="pairing">
@@ -36,7 +96,7 @@ function Pairing({
 function Upnext({
   upcoming: [active, onDeck, inTheHole],
 }: {
-  upcoming: [string, string][];
+  upcoming: (readonly [string, string])[];
 }) {
   return (
     <div className="upnext">
@@ -78,7 +138,6 @@ function Players({
 function AddPlayers({ addPlayer }: { addPlayer: (player: string) => void }) {
   return (
     <>
-      <h2>Controls</h2>
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -93,10 +152,8 @@ function AddPlayers({ addPlayer }: { addPlayer: (player: string) => void }) {
           }
         }}
       >
-        <label>
-          Add Player: <input name="player" type="text" />
-        </label>
-        <input type="submit" />
+        <input name="player" type="text" />
+        <input type="submit" value="Add player" />
       </form>
     </>
   );
@@ -122,66 +179,218 @@ function GameSetup({
 }
 
 function GameActive({
-  buzzleCounts,
   leaderboard,
   playRound,
   resetUpcoming,
   addPlayer,
-  showAddPlayers,
+  showExtraControls,
+  setShowExtraControls,
+  numberOfPlayersToShow,
+  setNumberOfPlayersToShow,
   upcoming,
+  setInEditMode,
 }: {
-  showAddPlayers: boolean;
-  buzzleCounts: [string, number][];
+  showExtraControls: boolean;
+  setShowExtraControls: (x: boolean) => void;
+  numberOfPlayersToShow: number | null;
+  setNumberOfPlayersToShow: (x: number | null) => void;
   leaderboard: [string, string, number][];
-  upcoming: [string, string][];
+  upcoming: (readonly [string, string])[];
   playRound: (player1: string, player2: string, time: number) => void;
   addPlayer: (player: string) => void;
   resetUpcoming: () => void;
+  setInEditMode: (x: boolean) => void;
 }) {
   return (
     <>
       <div className="main">
-        <Leaderboard times={leaderboard} />
-        <Upnext upcoming={upcoming} />
-      </div>
-      {showAddPlayers ? <AddPlayers addPlayer={addPlayer} /> : <></>}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
+        <div className="left">
+          <Upnext upcoming={upcoming} />
+          <div className="controls">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
 
-          const form = e.target as HTMLFormElement;
-          const formData = new FormData(form);
-          const formJson = Object.fromEntries(formData.entries());
-          form.reset();
-          const player1 = formJson.player1 as string | undefined;
-          const player2 = formJson.player2 as string | undefined;
-          const time = formJson.time as string;
-          if (time && player1 && player2) {
-            const timeNumber = fromMinutesAndSeconds(time);
-            if (timeNumber) {
-              playRound(player1, player2, timeNumber);
-            }
+                const form = e.target as HTMLFormElement;
+                const formData = new FormData(form);
+                const formJson = Object.fromEntries(formData.entries());
+                form.reset();
+                const player1 = formJson.player1 as string | undefined;
+                const player2 = formJson.player2 as string | undefined;
+                const time = formJson.time as string;
+                if (time && player1 && player2) {
+                  const timeNumber = fromMinutesAndSeconds(time);
+                  if (timeNumber) {
+                    playRound(player1, player2, timeNumber);
+                  }
+                }
+              }}
+            >
+              <label>
+                Player 1:{" "}
+                <input
+                  name="player1"
+                  type="text"
+                  defaultValue={upcoming[0][0]}
+                />
+              </label>
+              <label>
+                Player 2:{" "}
+                <input
+                  name="player2"
+                  type="text"
+                  defaultValue={upcoming[0][1]}
+                />
+              </label>
+              <label>
+                Time: <input name="time" type="text" />
+              </label>
+              <input type="submit" value="Add time" />
+            </form>
+            <hr />
+            <input
+              type="button"
+              value={showExtraControls ? "Less" : "More"}
+              onClick={() => setShowExtraControls(!showExtraControls)}
+            />
+            {showExtraControls ? (
+              <>
+                <AddPlayers addPlayer={addPlayer} />{" "}
+                <label>
+                  Number of records to show:
+                  <input
+                    name="numberToShow"
+                    type="number"
+                    defaultValue={numberOfPlayersToShow ?? ""}
+                    onChange={(e) => {
+                      e.preventDefault();
+                      const numberToShow = e.target.value;
+                      setNumberOfPlayersToShow(
+                        numberToShow == "" ? null : Number(numberToShow)
+                      );
+                    }}
+                  />
+                </label>
+                <input
+                  type="button"
+                  value="Reset up next"
+                  onClick={(e) => resetUpcoming()}
+                />
+                <input
+                  type="button"
+                  value="Enter edit mode"
+                  onClick={(e) => setInEditMode(true)}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+        <Leaderboard times={leaderboard} />
+      </div>
+    </>
+  );
+}
+
+export function EditMode({
+  playerList,
+  scores,
+  upcoming,
+  setPlayerList,
+  setScores,
+  setUpcoming,
+  exitEditMode,
+  fullReset,
+}: {
+  playerList: Set<string>;
+  scores: Map<[string, string], number>;
+  upcoming: (readonly [string, string])[];
+  setPlayerList: (x: Set<string>) => void;
+  setScores: (x: Map<[string, string], number>) => void;
+  setUpcoming: (x: [string, string][]) => void;
+  exitEditMode: () => void;
+  fullReset: () => void;
+}) {
+  const sortedPlayerList = [...playerList];
+  sortedPlayerList.sort();
+  const scoresToSort = [...scores];
+  scoresToSort.sort((a, b) => a[1] - b[1]);
+  const formattedScores = scoresToSort.map(
+    ([[a, b], t]) => `${a} & ${b} -- ${minutesAndSeconds(t)}`
+  );
+  const upcomingMapped = upcoming.map(([a, b]) => `${a} & ${b}`);
+
+  return (
+    <>
+      <div>
+        <h3>Players</h3>
+        <textarea
+          onChange={(change) =>
+            setPlayerList(
+              new Set(change.target.value.split("\n").map(cleanupString))
+            )
           }
-        }}
-      >
-        <label>
-          Player 1:{" "}
-          <input name="player1" type="text" defaultValue={upcoming[0][0]} />
-        </label>
-        <label>
-          Player 2:{" "}
-          <input name="player2" type="text" defaultValue={upcoming[0][1]} />
-        </label>
-        <label>
-          Time: <input name="time" type="text" />
-        </label>
-        <input type="submit" />
-      </form>
-      <input
-        type="button"
-        value="Reset up next"
-        onClick={(e) => resetUpcoming()}
-      />
+        >
+          {sortedPlayerList.join("\n")}
+        </textarea>
+      </div>
+      <div>
+        <h3>Leaderboard</h3>
+        <textarea
+          rows={formattedScores.length}
+          cols={50}
+          onChange={(change) => {
+            const newScores = change.target.value
+              .split("\n")
+              .map((row) => {
+                const match = row.match(
+                  /^([^\&]+)\&([^\-]+)--\s*([0-9:]+)\s*$/
+                );
+                if (match) {
+                  return [
+                    ordered([cleanupString(match[1]), cleanupString(match[2])]),
+                    fromMinutesAndSeconds(match[3]),
+                  ] as const;
+                }
+              })
+              .filter((x): x is [[string, string], number] => !!x);
+            setScores(new Map(newScores));
+          }}
+        >
+          {formattedScores.join("\n")}
+        </textarea>
+      </div>
+      <div>
+        <h3>Upcoming parings</h3>
+        <textarea
+          rows={3}
+          cols={50}
+          onChange={(change) => {
+            const newUpcoming = change.target.value
+              .split("\n")
+              .map((row) => {
+                const [a, b] = row.split("&");
+                if (!b) return;
+                return [cleanupString(a), cleanupString(b)] as const;
+              })
+              .filter((x): x is [string, string] => !!x);
+            setUpcoming(newUpcoming);
+          }}
+        >
+          {upcomingMapped.join("\n")}
+        </textarea>
+      </div>
+      <div>
+        <input type="button" value="Done" onClick={(e) => exitEditMode()} />
+      </div>
+      <div>
+        <input
+          type="button"
+          value="Clear leaderboard and reset game"
+          onClick={(e) => fullReset()}
+        />
+      </div>
     </>
   );
 }
@@ -193,6 +402,19 @@ export default function App() {
   const [state, setState] = useLocalStorageState("gameState", {
     defaultValue: () => new Game().toJSON(),
   });
+  const [inEditMode, setInEditMode] = useLocalStorageState("inEditMode", {
+    defaultValue: false,
+  });
+  const [numberOfPlayersToShow, setNumberOfPlayersToShow] =
+    useLocalStorageState<number | null>("numberOfPlayersToShow", {
+      defaultValue: 10,
+    });
+  const [showExtraControls, setShowExtraControls] = useLocalStorageState(
+    "showExtraControls",
+    {
+      defaultValue: false,
+    }
+  );
 
   const setGameState = () => {
     game.upcoming;
@@ -208,31 +430,53 @@ export default function App() {
   const addPlayer = (player: string) =>
     withGame((game) => game.addPlayer(player));
 
-  if (!hasBegun) {
+  if (inEditMode) {
+    return (
+      <EditMode
+        playerList={game.players}
+        scores={game.buzzles}
+        upcoming={game.upcoming}
+        setPlayerList={(players) => withGame((g) => (g.players = players))}
+        setScores={(scores) => withGame((g) => (g.buzzles = scores))}
+        setUpcoming={(upcoming) => withGame((g) => g.resetUpcoming(upcoming))}
+        exitEditMode={() => setInEditMode(false)}
+        fullReset={() => {
+          setState(new Game().toJSON());
+          setHasBegun(false);
+          setInEditMode(false);
+        }}
+      />
+    );
+  } else if (!hasBegun || game.players.size < minNumberPlayers) {
     return (
       <GameSetup
         buzzleCounts={game.buzzleCounts()}
-        startGame={() =>
-          withGame(() => {
-            setHasBegun(true);
-          })
-        }
+        startGame={() => {
+          if (game.players.size >= minNumberPlayers) {
+            withGame(() => setHasBegun(true));
+          }
+        }}
         addPlayer={addPlayer}
       />
     );
   } else {
     return (
-      <GameActive
-        showAddPlayers={true}
-        buzzleCounts={game.buzzleCounts()}
-        leaderboard={game.leaderboard(10)}
-        addPlayer={addPlayer}
-        upcoming={game.upcoming}
-        playRound={(player1, player2, time) =>
-          withGame((game) => game.addRound([player1, player2], time))
-        }
-        resetUpcoming={() => withGame((game) => game.resetUpcoming())}
-      />
+      <>
+        <GameActive
+          showExtraControls={showExtraControls}
+          setShowExtraControls={setShowExtraControls}
+          numberOfPlayersToShow={numberOfPlayersToShow}
+          setNumberOfPlayersToShow={setNumberOfPlayersToShow}
+          leaderboard={game.leaderboard(numberOfPlayersToShow)}
+          addPlayer={addPlayer}
+          upcoming={game.upcoming}
+          playRound={(player1, player2, time) =>
+            withGame((game) => game.addRound([player1, player2], time))
+          }
+          resetUpcoming={() => withGame((game) => game.resetUpcoming())}
+          setInEditMode={setInEditMode}
+        />
+      </>
     );
   }
 }
--- src/game.spec.ts
+++ src/game.spec.ts
@@ -10,3 +10,30 @@ test("can add players to the game", () => {
   game.addPlayer("D");
   expect(game.players.size).toBe(4);
 });
+
+test("can generate a bunch of rounds", () => {
+  const game = new Game();
+  game.addPlayer("A");
+  game.addPlayer("B");
+  game.addPlayer("C");
+  game.addPlayer("D");
+  game.addPlayer("E");
+  game.addPlayer("F");
+  game.addPlayer("G");
+  game.addPlayer("H");
+  game.addPlayer("I");
+  game.addPlayer("J");
+  game.addPlayer("K");
+  game.addPlayer("L");
+  game.addPlayer("M");
+  game.addPlayer("N");
+
+  for (let iteration = 0; iteration < 100; iteration++) {
+    if (game.upcoming.length !== 3) {
+      console.log("oops");
+    }
+    expect(game.upcoming.length).toBe(3);
+    console.log(iteration);
+    game.addRound(game.upcoming[0], 100);
+  }
+});
